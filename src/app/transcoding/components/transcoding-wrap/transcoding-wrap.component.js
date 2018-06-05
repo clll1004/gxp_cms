c622f111cd4e4a770065f8b3dfac84bc98f5d908
chealin <clll1004@gomcorp.com>
2018-06-05 10:53:18 +0900

    upload

--- src/app/transcoding/components/transcoding-wrap/transcoding-wrap.component.js
+++ src/app/transcoding/components/transcoding-wrap/transcoding-wrap.component.js
@@ -1,9 +1,2 @@
-// $.ajax ({//
-//     "url" : "http://183.110.11.49/adm/customer/list",
-//     cache : false,//
-//     success : function (html) {//
-//         alert(html);//
-//     }//
-// });
 
 alert('r');
