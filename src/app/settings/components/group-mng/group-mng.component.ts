805552d168c729a163064c138fdbae2d3d8884aa
chealin <clll1004@gomcorp.com>
2018-07-18 19:47:33 +0900

    imp: settings group management

--- src/app/settings/components/group-mng/group-mng.component.ts
+++ src/app/settings/components/group-mng/group-mng.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Http } from '@angular/http';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'group-mng',
@@ -6,24 +8,51 @@ import { Component } from '@angular/core';
     styleUrls: ['../../settings.component.css']
 })
 
+export class GroupMngComponent implements OnInit {
+    @Input() groupData: any[];
+    @Input() transOptions: any[] = [];
 
-export class GroupMngComponent {
-    transUse: any[];
-    transFPS: any[];
-    selectedOption: any[];
-    selectedOption2: any[];
-    selectedFPSOption: any[];
-    selectedFPSOption2: any[];
+    public transOptionsCols: any[] = [
+        {field: '', header: 'No', width: '5%'},
+        {field: '', header: '옵션명', width: '10%'},
+        {field: '', header: '사용여부', width: '10%'},
+        {field: '', header: '파일접미어', width: '10%'},
+        {field: '', header: 'FPS', width: '15%'},
+        {field: '', header: '비디오비트율', width: '10%'},
+        {field: '', header: '오디오비트율', width: '10%'},
+        {field: '', header: '해상도(가로)', width: '10%'},
+        {field: '', header: '해상도(세로)', width: '10%'},
+        {field: '', header: '수정', width: '10%'},
+    ]
 
-    constructor() {
-        this.transUse = [
-            {label:'사용여부', value:null},
-            {label:'사용', value:true},
-            {label:'사용안함', value:false}
-        ];
-        this.transFPS = [
-            {label:'FPS', value:null},
-            {label:'29.97', value:{id:1, name: '29.97', code: '29.97'}}
-        ];
+    public transOptionform: FormGroup;
+
+    transUse: any[] = [
+        {label:'사용여부', value:null},
+        {label:'사용', value:true},
+        {label:'사용안함', value:false}
+    ];
+    transFPS: any[] = [
+        {label:'FPS', value:null},
+        {label:'29.97', value:{id:1, name: '29.97', code: '29.97'}}
+    ];
+
+    constructor(private formBuilder: FormBuilder,private http: Http) { }
+
+    ngOnInit() {
+        this.transOptionform = this.formBuilder.group({
+            'gto_seq': new FormControl(null),
+            'gto_use_yn': new FormControl(null),
+            'gto_file_suffix': new FormControl(null),
+            'gto_frame_rate': new FormControl(null),
+            'gto_video_bitrate': new FormControl(null),
+            'gto_audio_bitrate': new FormControl(null),
+            'gto_dst_width': new FormControl(null),
+            'gto_dst_height': new FormControl(null),
+        });
+    }
+
+    showForms() {
+
     }
 }
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,9 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import { ActivatedRoute, Params} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from "@angular/router";
 import { Http } from "@angular/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs/Observable";
-import { TreeNode } from "primeng/api";
+import { TreeNode } from 'primeng/api';
 
 @Component({
     selector: 'settings',
@@ -11,34 +9,51 @@ import { TreeNode } from "primeng/api";
     styleUrls: ['./settings.component.css']
 })
 
-@Injectable()
 export class SettingsComponent implements OnInit {
-
+    public groupList: TreeNode[];
+    public selectGroup: TreeNode;
+    public groupData: any[] = [];
+    public transOptions: any[] = [];
+    public groupSeq:string = '1';
     public params:Params;
-
-    files: TreeNode[];
-    selectedFile: TreeNode;
 
     constructor(private activatedRoute: ActivatedRoute, private http: Http) {
         this.activatedRoute.params.subscribe( (params) => {
             this.params = params;
         });
 
-        let obj:any;
-        this.getFiles().subscribe((data) => {
-            obj = data;
-        }, error => {
-            console.log(error);
-        });
     }
-
-    getFiles(): Observable<any> {
-        return this.http.get('src/app/settings/group-data.json');
-        // .toPromise()
-        // .then(res => <TreeNode[]> res.json().data);
-    }
-
     ngOnInit() {
-        // this.getFiles().then(files => this.files = files);
+        this.load();
+    }
+
+    load() {
+        this.loadGroupList();
+        this.loadGroupData();
+    }
+
+    loadGroupList() {
+        return this.http.get('http://localhost:8080/src/app/settings/group-data.json')
+          .toPromise()
+          .then((res) => {
+              this.groupList = <TreeNode[]> res.json().data;
+          });
+    }
+    getGroupSeq() {
+        if (this.selectGroup.label === 'GXP') {
+            this.groupSeq = '1';
+        } else if (this.selectGroup.label === 'grp2') {
+            this.groupSeq = '2';
+        }
+        this.loadGroupData();
+    }
+
+    loadGroupData() {
+        return this.http.get('http://183.110.11.49/cms/setting/group/' + this.groupSeq)
+          .toPromise()
+          .then((cont) => {
+              this.groupData = JSON.parse(cont['_body']).grp;
+              this.transOptions = JSON.parse(cont['_body']).tcd;
+          });
     }
 }
