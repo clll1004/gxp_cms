01f94badd4ddfbdd96dab2f6a65c7d0acdc4f567
chealin <clll1004@gomcorp.com>
2018-08-02 16:08:30 +0900

    bugfix: login layout error fix(2033)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router, NavigationEnd } from "@angular/router";
 import { LoginService } from "./login/login.service";
 
@@ -12,11 +12,15 @@ import '../assets/css/styles.css';
     providers: [ LoginService ]
 })
 
+@HostListener('window:onbeforeunload', ['$event'])
+
 export class AppComponent implements OnInit {
     public data: any;
     public isShow: boolean = false;
 
-    constructor(private router: Router, private loginService: LoginService) {}
+    constructor(private router: Router, private loginService: LoginService) {
+        this.onBeforeUnload();
+    }
 
     ngOnInit() {
         this.load();
@@ -31,9 +35,15 @@ export class AppComponent implements OnInit {
     initLayoutStatus() {
         this.router.events.subscribe((event) => {
             if(event instanceof NavigationEnd) {
-                this.isShow = this.loginService.getLoginStatus();
+                this.isShow = !!this.loginService.getCookie('userInfo');
             }
         });
+    }
+
+    onBeforeUnload() {
+        this.loginService.deleteCookie('userInfo');
+        this.loginService.deleteCookie('usr_seq');
+        this.loginService.deleteCookie('grp_seq');
     }
 }
 
