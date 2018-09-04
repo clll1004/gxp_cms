1d1410542de57d4bb81c4e4d00a17bc14927e123
chealin <clll1004@gomcorp.com>
2018-09-04 12:24:02 +0900

    imp: 통계 menu 동작 개선 (3059)

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -21,20 +21,18 @@ export class StatisticsComponent implements OnInit {
 
   showSubLnb(e) {
     const target = e.currentTarget;
-    const length = target.parentNode.children.length;
+    const id = target.getAttribute('id');
+    const temp:any[] = target.parentNode.children;
+    const length = temp.length;
 
     for (let i = 0 ; i < length ; i += 1) {
-      if (target.parentElement.children[i].localName === 'li') {
-        if (target.parentElement.children[i].getAttribute('class') === 'has-sub on') {
-          target.parentElement.children[i].setAttribute('class', 'has-sub');
-          target.parentElement.children[i].children[0].setAttribute('class', 'fas fa-angle-down');
-        }
+      if (temp[i].getAttribute('class') === 'has-sub on') {
+        temp[i].setAttribute('class', 'has-sub');
+        temp[i].children[0].setAttribute('class', 'fas fa-angle-down');
+      } else if (temp[i].getAttribute('id') === id && id !== 'dashboard') {
+        target.setAttribute('class', 'has-sub on');
+        target.children[0].setAttribute('class', 'fas fa-angle-up');
       }
-    }
-
-    if (target.getAttribute('class')) {
-      target.setAttribute('class', 'has-sub on');
-      target.children[0].setAttribute('class', 'fas fa-angle-up');
     }
   }
 }
