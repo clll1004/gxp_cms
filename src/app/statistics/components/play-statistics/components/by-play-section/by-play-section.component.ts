1a2988afa649eaef8276e83c5d4866d738a852a6
chealin <clll1004@gomcorp.com>
2018-09-07 10:18:17 +0900

    fix: 재생구간 테이블 데이터 정리(3162)

--- src/app/statistics/components/play-statistics/components/by-play-section/by-play-section.component.ts
+++ src/app/statistics/components/play-statistics/components/by-play-section/by-play-section.component.ts
@@ -28,6 +28,8 @@ export class ByPlaySectionComponent implements OnInit, OnChanges {
 
   public comparePopularSectionValue:any[] = [];
   public compareLeaveSectionValue:any[] = [];
+  public comparePopularSectionAverageValue:any[] = [];
+  public compareLeaveSectionAverageValue:any[] = [];
   /*table cols*/
   public playTimeStatisticsCols:any[] = [
     { header: 'No', field: 'no' },
@@ -58,6 +60,7 @@ export class ByPlaySectionComponent implements OnInit, OnChanges {
   /*table data*/
   public playTimeStatisticsDatas:any[] = [];
   public compareSectionDatas:any[] = [];
+  public compareSectionAverageData:any[] = [];
   /*chart data*/
   public compareLength:any[] = [];
   public comparePlaySectionData:any[] = [];
@@ -106,22 +109,30 @@ export class ByPlaySectionComponent implements OnInit, OnChanges {
       this.tableLists.push({ date: item.getFullYear() + '-' + (item.getMonth() + 1) + '-' + item.getDate(), empty: '0' });
     });
     this.playTimeStatisticsDatas = [];
-    let i = 0;
-    this.dateArray.forEach(() => {
-      i += 1;
-      this.playTimeStatisticsDatas.push(
-        { no: i,
-          groupName: 'GXP',
-          folderName: '사회',
-          contentsName: '공인중개사 6강',
-          videoTime: 150,
-          playTime: '08:13:23',
-          playCount: 30,
-          playRate: '10%',
-          averagePlayTime: '03:20:13',
-          updateDate: '2018-09-09',
-        });
-    });
+    this.playTimeStatisticsDatas.push(
+      { no: 1,
+        groupName: 'GXP',
+        folderName: '사회',
+        contentsName: '토익1강',
+        videoTime: 30,
+        playTime: '08:13:23',
+        playCount: Math.floor(Math.random() * 10000),
+        playRate: 10,
+        averagePlayTime: '03:20:13',
+        updateDate: '2018-09-09',
+      });
+    this.playTimeStatisticsDatas.push(
+      { no: 2,
+        groupName: 'GXP',
+        folderName: '사회',
+        contentsName: '토익2강',
+        videoTime: 30,
+        playTime: '08:13:23',
+        playCount: Math.floor(Math.random() * 10000),
+        playRate: 10,
+        averagePlayTime: '03:20:13',
+        updateDate: '2018-09-09',
+      });
   }
 
   getDateArray(startDate, endDate) {
@@ -155,25 +166,31 @@ export class ByPlaySectionComponent implements OnInit, OnChanges {
     }
     this.compareSectionDatas = [];
     let i = 0;
-    this.tempCompareItems.forEach((item) => {
-      this.compareSectionDatas.push({
-        no: item.no,
-        contentsName: item.contentsName,
-        date: this.selectDuration.date[0] + ' ~ ' + this.selectDuration.date[1],
-        p10: Math.floor(Math.random() * 1000),
-        p20: Math.floor(Math.random() * 1000),
-        p30: Math.floor(Math.random() * 1000),
-        p40: Math.floor(Math.random() * 1000),
-        p50: Math.floor(Math.random() * 1000),
-        p60: Math.floor(Math.random() * 1000),
-        p70: Math.floor(Math.random() * 1000),
-        p80: Math.floor(Math.random() * 1000),
-        p90: Math.floor(Math.random() * 1000),
-        p100: Math.floor(Math.random() * 1000),
-        index: i,
+    let tempCompareDatas:any[] = [];
+    this.dateArray.forEach((date:Date) => {
+      tempCompareDatas = [];
+      this.tempCompareItems.forEach((item) => {
+        tempCompareDatas.push({
+          no: item.no,
+          contentsName: item.contentsName,
+          date: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()),
+          p10: Math.floor(Math.random() * 1000),
+          p20: Math.floor(Math.random() * 1000),
+          p30: Math.floor(Math.random() * 1000),
+          p40: Math.floor(Math.random() * 1000),
+          p50: Math.floor(Math.random() * 1000),
+          p60: Math.floor(Math.random() * 1000),
+          p70: Math.floor(Math.random() * 1000),
+          p80: Math.floor(Math.random() * 1000),
+          p90: Math.floor(Math.random() * 1000),
+          p100: Math.floor(Math.random() * 1000),
+          index: i,
+        });
+        i += 1;
       });
-      i += 1;
+      this.compareSectionDatas.push(tempCompareDatas);
     });
+    this.setAverageData();
     this.getPopularOrLeaveSection();
     this.setCompareSectionChartData();
   }
@@ -198,19 +215,125 @@ export class ByPlaySectionComponent implements OnInit, OnChanges {
   getPopularOrLeaveSection() {
     this.comparePopularSectionValue = [];
     this.compareLeaveSectionValue = [];
-    const temp:any[] = [];
-    this.compareSectionDatas.forEach((item) => {
-      temp.push([item['p10'], item['p20'], item['p30'], item['p40'], item['p50'], item['p60'], item['p70'], item['p80'], item['p90'], item['p100']]);
-    });
-    temp.forEach((item) => {
-      let popular:number = item[0];
-      let leave:number = item[0];
-      item.forEach((data) => {
-        popular = popular < data ? data : popular;
-        leave = leave > data ? data : leave;
+    const temp1:any[] = [];
+    let temp2:any[] = [];
+    this.compareSectionDatas.forEach((item:any[]) => {
+      temp2 = [];
+      item.forEach((value) => {
+        temp2.push([value['p10'], value['p20'], value['p30'], value['p40'], value['p50'], value['p60'], value['p70'], value['p80'], value['p90'], value['p100']]);
       });
-      this.comparePopularSectionValue.push(popular);
-      this.compareLeaveSectionValue.push(leave);
+      temp1.push(temp2);
+    });
+    temp1.forEach((item:any[]) => {
+      item.forEach((item2:any[]) => {
+        let popular:number = item2[0];
+        let leave:number = item2[0];
+        item2.forEach((value) => {
+          popular = popular < value ? value : popular;
+          leave = leave > value ? value : leave;
+        });
+        this.comparePopularSectionValue.push(popular);
+        this.compareLeaveSectionValue.push(leave);
+      });
+    });
+
+    this.comparePopularSectionAverageValue = [];
+    this.compareLeaveSectionAverageValue = [];
+    const averageTemp1:any[] = [];
+    let averageTemp2:any[] = [];
+    this.compareSectionAverageData.forEach((item:any[]) => {
+      averageTemp2 = [];
+      averageTemp2.push([item['p10'], item['p20'], item['p30'], item['p40'], item['p50'], item['p60'], item['p70'], item['p80'], item['p90'], item['p100']]);
+      averageTemp1.push(averageTemp2);
+    });
+    averageTemp1.forEach((item:any[]) => {
+      item.forEach((item2:any[]) => {
+        let popular: number = item2[0];
+        let leave: number = item2[0];
+        item2.forEach((value) => {
+          popular = popular < value ? value : popular;
+          leave = leave > value ? value : leave;
+        });
+        this.comparePopularSectionAverageValue.push(popular);
+        this.compareLeaveSectionAverageValue.push(leave);
+      });
+    });
+  }
+
+  setAverageData() {
+    this.compareSectionAverageData = [];
+    const tempData:object = {
+      p10: 0,
+      p20: 0,
+      p30: 0,
+      p40: 0,
+      p50: 0,
+      p60: 0,
+      p70: 0,
+      p80: 0,
+      p90: 0,
+      p100: 0,
+    };
+    const tempData2:object = {
+      p10: 0,
+      p20: 0,
+      p30: 0,
+      p40: 0,
+      p50: 0,
+      p60: 0,
+      p70: 0,
+      p80: 0,
+      p90: 0,
+      p100: 0,
+    };
+    for (let j = 0 ; j < this.compareItems.length ; j += 1) {
+      for (let i = 0 ; i < this.compareSectionDatas.length ; i += 1) {
+        if (j === 0) {
+          tempData['index'] = j;
+          tempData['date'] = this.selectDuration['date'][0] + ' ~ ' + this.selectDuration['date'][1];
+          tempData['contentsName'] = this.compareSectionDatas[i][j]['contentsName'];
+          tempData['p10'] = tempData['p10'] + this.compareSectionDatas[i][j]['p10'];
+          tempData['p20'] = tempData['p20'] + this.compareSectionDatas[i][j]['p20'];
+          tempData['p30'] = tempData['p30'] + this.compareSectionDatas[i][j]['p30'];
+          tempData['p40'] = tempData['p40'] + this.compareSectionDatas[i][j]['p40'];
+          tempData['p50'] = tempData['p50'] + this.compareSectionDatas[i][j]['p50'];
+          tempData['p60'] = tempData['p60'] + this.compareSectionDatas[i][j]['p60'];
+          tempData['p70'] = tempData['p70'] + this.compareSectionDatas[i][j]['p70'];
+          tempData['p80'] = tempData['p80'] + this.compareSectionDatas[i][j]['p80'];
+          tempData['p90'] = tempData['p90'] + this.compareSectionDatas[i][j]['p70'];
+          tempData['p100'] = tempData['p100'] + this.compareSectionDatas[i][j]['p100'];
+        } else {
+          tempData2['index'] = j;
+          tempData2['date'] = this.selectDuration['date'][0] + ' ~ ' + this.selectDuration['date'][1];
+          tempData2['contentsName'] = this.compareSectionDatas[i][j]['contentsName'];
+          tempData2['p10'] = tempData2['p10'] + this.compareSectionDatas[i][j]['p10'];
+          tempData2['p20'] = tempData2['p20'] + this.compareSectionDatas[i][j]['p20'];
+          tempData2['p30'] = tempData2['p30'] + this.compareSectionDatas[i][j]['p30'];
+          tempData2['p40'] = tempData2['p40'] + this.compareSectionDatas[i][j]['p40'];
+          tempData2['p50'] = tempData2['p50'] + this.compareSectionDatas[i][j]['p50'];
+          tempData2['p60'] = tempData2['p60'] + this.compareSectionDatas[i][j]['p60'];
+          tempData2['p70'] = tempData2['p70'] + this.compareSectionDatas[i][j]['p70'];
+          tempData2['p80'] = tempData2['p80'] + this.compareSectionDatas[i][j]['p80'];
+          tempData2['p90'] = tempData2['p90'] + this.compareSectionDatas[i][j]['p70'];
+          tempData2['p100'] = tempData2['p100'] + this.compareSectionDatas[i][j]['p100'];
+        }
+      }
+    }
+    this.compareSectionAverageData.push(tempData);
+    if (tempData2['p10']) {
+      this.compareSectionAverageData.push(tempData2);
+    }
+    this.compareSectionAverageData.forEach((item) => {
+      item['p10'] = Math.floor(item['p10'] / this.dateArray.length);
+      item['p20'] = Math.floor(item['p20'] / this.dateArray.length);
+      item['p30'] = Math.floor(item['p30'] / this.dateArray.length);
+      item['p40'] = Math.floor(item['p40'] / this.dateArray.length);
+      item['p50'] = Math.floor(item['p50'] / this.dateArray.length);
+      item['p60'] = Math.floor(item['p60'] / this.dateArray.length);
+      item['p70'] = Math.floor(item['p70'] / this.dateArray.length);
+      item['p80'] = Math.floor(item['p80'] / this.dateArray.length);
+      item['p90'] = Math.floor(item['p90'] / this.dateArray.length);
+      item['p100'] = Math.floor(item['p100'] / this.dateArray.length);
     });
   }
 
@@ -220,16 +343,16 @@ export class ByPlaySectionComponent implements OnInit, OnChanges {
     const tempData:any[] = [];
     const tborderColors:any[] = ['#ffcdd2', '#e1bee7'];
     const tbackgroundColors:any[] = ['rgba(255,205,210,.2)', 'rgba(225,190,231,.2)'];
-    this.compareSectionDatas.forEach((item) => {
+    this.compareSectionAverageData.forEach((item) => {
       tempData.push([item['p10'], item['p20'], item['p30'], item['p40'], item['p50'], item['p60'], item['p70'], item['p80'], item['p90'], item['p100']]);
     });
     let i = 0;
-    this.compareSectionDatas.forEach((item) => {
+    this.compareSectionAverageData.forEach((item) => {
       this.compareLength.push(i);
       this.comparePlaySectionData.push({
         labels: ['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'],
         datasets: [{
-          label: item['no'],
+          label: item['contentsName'],
           data: tempData[i],
           fill: true,
           borderColor: tborderColors[i],
