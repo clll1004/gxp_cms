1a38ee14538df3101cac0c960b5123a801c54287
chealin <clll1004@gomcorp.com>
2018-09-26 19:42:59 +0900

    fix: 통계 메뉴 동작 개선(3561)

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -35,10 +35,10 @@ export class StatisticsComponent implements OnInit {
         });
         if (item.getAttribute('id') === this.params['id']) {
           item.setAttribute('class', 'has-sub on');
-          item.children[0].setAttribute('class', 'fas fa-angle-up');
+          item.children[0].children[0].setAttribute('class', 'fas fa-angle-up');
         } else {
           item.setAttribute('class', 'has-sub');
-          item.children[0].setAttribute('class', 'fas fa-angle-down');
+          item.children[0].children[0].setAttribute('class', 'fas fa-angle-down');
         }
       }
     });
@@ -51,23 +51,23 @@ export class StatisticsComponent implements OnInit {
     const length = temp.length;
 
     for (let i = 0 ; i < length ; i += 1) {
-      if (temp[i].getAttribute('class') === 'has-sub on') {
+      if (temp[i].getAttribute('class') === 'has-sub' || temp[i].getAttribute('class') === 'has-sub on') {
         temp[i].setAttribute('class', 'has-sub');
-        temp[i].children[0].setAttribute('class', 'fas fa-angle-down');
-      } else if (temp[i].getAttribute('id') === id && id !== 'visitor-statistics' && id !== 'dashboard') {
-        target.setAttribute('class', 'has-sub on');
-        target.children[0].setAttribute('class', 'fas fa-angle-up');
+        temp[i].children[0].children[0].setAttribute('class', 'fas fa-angle-down');
       }
     }
 
-    const lnb = document.getElementsByClassName('lnb-sec')[0].childNodes[0].childNodes;
-    [].forEach.call(lnb, (item) => {
-      if (item.getAttribute('class') === 'sub-lnb') {
-        item.childNodes.forEach((li) => {
-          li.setAttribute('class', '');
-        });
-      }
-    });
+    if (target.getAttribute('class') === 'has-sub') {
+      target.setAttribute('class', 'has-sub on');
+      target.children[0].children[0].setAttribute('class', 'fas fa-angle-up');
+      [].forEach.call(target.nextSibling.children, (item) => {
+        item.setAttribute('class', '');
+      });
+      target.nextSibling.children[0].setAttribute('class', 'on');
+    } else if (target.getAttribute('class') === 'has-sub on') {
+      target.setAttribute('class', 'has-sub');
+      target.children[0].children[0].setAttribute('class', 'fas fa-angle-down');
+    }
   }
 
   isSubOn(e) {
