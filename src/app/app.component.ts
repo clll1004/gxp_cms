ad6905975cb92b3c208c606bd6a2512dd5f592be
Yoosoo Won <Sihyun@Yoosooui-MacBook-Pro.local>
2019-01-02 09:48:19 +0900

    imp: primeNG 새로운 디자인 템플릿을 적용한다(6344)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,163 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { LoginService } from './services/apis/cms/login/login.service';
-import '../assets/css/styles.css';
+import {Component, AfterViewInit, ElementRef, Renderer2, ViewChild, OnDestroy} from '@angular/core';
+import {ScrollPanel} from 'primeng/primeng';
 
 @Component({
-  selector: 'app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']})
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
 
-export class AppComponent implements OnInit {
-  public isLayoutShow: boolean = false;
+  layoutMode = 'static';
 
-  constructor(private router: Router, private loginService: LoginService) { }
+  megaMenuMode = 'dark';
 
-  ngOnInit() {
-    this.loginService.checkUserInfo();
+  menuMode = 'light';
 
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        if (event.url === '/' || event.url === '/login') {
-          this.loginService.logout();
-          this.isLayoutShow = false;
-        } else {
-          this.isLayoutShow = true;
-        }
+  profileMode = 'inline';
+
+  topbarMenuActive: boolean;
+
+  overlayMenuActive: boolean;
+
+  staticMenuDesktopInactive: boolean;
+
+  staticMenuMobileActive: boolean;
+
+  layoutMenuScroller: HTMLDivElement;
+
+  menuClick: boolean;
+
+  topbarItemClick: boolean;
+
+  activeTopbarItem: any;
+
+  resetMenu: boolean;
+
+  rightPanelActive: boolean;
+
+  rightPanelClick: boolean;
+
+  megaMenuActive: boolean;
+
+  megaMenuClick: boolean;
+
+  @ViewChild('layoutMenuScroller') layoutMenuScrollerViewChild: ScrollPanel;
+
+  constructor(public renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {this.layoutMenuScrollerViewChild.moveBar(); }, 100);
+  }
+
+  onLayoutClick() {
+    if (!this.topbarItemClick) {
+      this.activeTopbarItem = null;
+      this.topbarMenuActive = false;
+    }
+
+      if (!this.rightPanelClick) {
+          this.rightPanelActive = false;
       }
-    });
+
+      if (!this.megaMenuClick) {
+          this.megaMenuActive = false;
+      }
+
+    if (!this.menuClick) {
+      if (this.overlayMenuActive || this.staticMenuMobileActive) {
+        this.hideOverlayMenu();
+      }
+    }
+
+    this.topbarItemClick = false;
+    this.menuClick = false;
+      this.rightPanelClick = false;
+      this.megaMenuClick = false;
+  }
+
+  onMenuButtonClick(event) {
+    this.menuClick = true;
+    this.topbarMenuActive = false;
+
+    if (this.layoutMode === 'overlay') {
+      this.overlayMenuActive = !this.overlayMenuActive;
+    } else {
+      if (this.isDesktop()) {
+        this.staticMenuDesktopInactive = !this.staticMenuDesktopInactive; } else {
+        this.staticMenuMobileActive = !this.staticMenuMobileActive; }
+    }
+
+    event.preventDefault();
+  }
+
+  onMenuClick($event) {
+    this.menuClick = true;
+    this.resetMenu = false;
+  }
+
+  onTopbarMenuButtonClick(event) {
+    this.topbarItemClick = true;
+    this.topbarMenuActive = !this.topbarMenuActive;
+
+    this.hideOverlayMenu();
+
+    event.preventDefault();
+  }
+
+  onTopbarItemClick(event, item) {
+    this.topbarItemClick = true;
+
+    if (this.activeTopbarItem === item) {
+      this.activeTopbarItem = null; } else {
+      this.activeTopbarItem = item; }
+
+    event.preventDefault();
+  }
+
+  onTopbarSubItemClick(event) {
+    event.preventDefault();
+  }
+
+    onRightPanelButtonClick(event) {
+        this.rightPanelClick = true;
+        this.rightPanelActive = !this.rightPanelActive;
+        event.preventDefault();
+    }
+
+    onRightPanelClick() {
+        this.rightPanelClick = true;
+    }
+
+    onMegaMenuButtonClick(event) {
+        this.megaMenuClick = true;
+        this.megaMenuActive = !this.megaMenuActive;
+        event.preventDefault();
+    }
+
+    onMegaMenuClick() {
+        this.megaMenuClick = true;
+    }
+
+  hideOverlayMenu() {
+    this.overlayMenuActive = false;
+    this.staticMenuMobileActive = false;
+  }
+
+  isTablet() {
+    const width = window.innerWidth;
+    return width <= 1024 && width > 640;
+  }
+
+  isDesktop() {
+    return window.innerWidth > 1024;
+  }
+
+  isMobile() {
+    return window.innerWidth <= 640;
+  }
+
+  isOverlay() {
+    return this.layoutMode === 'overlay';
   }
 }
-
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-import './style.scss';
-
-if (process.env.NODE_ENV === 'production') {
-  enableProdMode();
+if (environment.production) {
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.log(err));
+    .catch(err => console.error(err));
