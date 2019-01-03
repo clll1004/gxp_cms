2c3b41093ceef91157a69cebf53ed6771203dd40
chealin <clll1004@gomcorp.com>
2019-01-03 12:22:20 +0900

    fix: 대시보드 UI(6404)

--- src/app/demo/view/dashboarddemo.component.ts
+++ src/app/demo/view/dashboarddemo.component.ts
@@ -1,86 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-import { CarService } from '../service/carservice';
-import { EventService } from '../service/eventservice';
-import { Car } from '../domain/car';
-import { SelectItem } from 'primeng/primeng';
-import { MenuItem } from 'primeng/primeng';
 import { BreadcrumbService } from '../../breadcrumb.service';
 
 @Component({
-    templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
 })
 export class DashboardDemoComponent implements OnInit {
+  public lineData:any;
+  public barData:any;
+  public pieData:any;
+  public rangeDates:Date[] = [new Date(), new Date()];
 
-    cities: SelectItem[];
+  constructor(private breadcrumbService: BreadcrumbService) {
+    this.breadcrumbService.setItems([
+      { label: '대시보드', routerLink: [''] }
+    ]);
+  }
 
-    cars: Car[];
+  ngOnInit() {
+    this.lineData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: '#ff5f00'
+        },
+        {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          borderColor: '#f37931'
+        }
+      ]
+    };
 
-    cols: any[];
+    this.barData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: '#ff5f00',
+          borderColor: '#ff5f00',
+          data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label: 'My Second dataset',
+          backgroundColor: '#ffcbba',
+          borderColor: '#ffcbba',
+          data: [28, 48, 40, 19, 86, 27, 90]
+        }
+      ]
+    };
 
-    chartData: any;
-
-    events: any[];
-
-    selectedCity: any;
-
-    selectedCar: Car;
-
-    items: MenuItem[];
-
-    fullcalendarOptions: any;
-
-    constructor(private carService: CarService, private eventService: EventService, private breadcrumbService: BreadcrumbService) {
-        this.breadcrumbService.setItems([
-            { label: 'Dashboard', routerLink: [''] }
-        ]);
-    }
-
-    ngOnInit() {
-        this.carService.getCarsMedium().then(cars => this.cars = cars);
-
-        this.cols = [
-            { field: 'vin', header: 'Vin' },
-            { field: 'year', header: 'Year' },
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' }
-        ];
-
-        this.eventService.getEvents().then(events => { this.events = events; });
-
-        this.cities = [];
-        this.cities.push({ label: 'Select City', value: null });
-        this.cities.push({ label: 'New York', value: { id: 1, name: 'New York', code: 'NY' } });
-        this.cities.push({ label: 'Rome', value: { id: 2, name: 'Rome', code: 'RM' } });
-        this.cities.push({ label: 'London', value: { id: 3, name: 'London', code: 'LDN' } });
-        this.cities.push({ label: 'Istanbul', value: { id: 4, name: 'Istanbul', code: 'IST' } });
-        this.cities.push({ label: 'Paris', value: { id: 5, name: 'Paris', code: 'PRS' } });
-
-        this.chartData = {
-            labels: ['0', '1', '2', '3', '4', '5', '6'],
-            datasets: [
-                {
-                    label: 'First Dataset',
-                    data: [, 6, 3, 2, 7, 9, ],
-                    fill: false,
-                    borderColor: '#FFC107'
-                },
-                {
-                    label: 'Second Dataset',
-                    data: [, 2, 1, 3, 6, 8, ],
-                    fill: false,
-                    borderColor: '#03A9F4'
-                }
-            ]
-        };
-
-        this.items = [
-            { label: 'Save', icon: 'fa fa-check' },
-            { label: 'Update', icon: 'fa fa-refresh' },
-            { label: 'Delete', icon: 'fa fa-trash' }
-        ];
-
-        this.fullcalendarOptions = {
-            defaultDate: '2016-01-12'
-        };
-    }
+    this.pieData = {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: [
+            '#ff5f00',
+            '#ffcbba',
+            '#f37931'
+          ]
+        }]
+    };
+  };
 }
+
