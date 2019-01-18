b657bc90c6d89600e4b264575d79c6c482c02dfe
chealin <clll1004@gomcorp.com>
2019-01-18 14:24:52 +0900

    imp: 광고전송이력 리스트 UI(6687)

--- src/app/demo/view/interactive-service/components/ad-manage/components/transmit-history/components/history-list/history-list.component.ts
+++ src/app/demo/view/interactive-service/components/ad-manage/components/transmit-history/components/history-list/history-list.component.ts
@@ -10,6 +10,20 @@ import { BreadcrumbService } from '../../../../../../../../../breadcrumb.service
   templateUrl: './history-list.component.html'})
 
 export class HistoryListComponent implements OnInit {
+  public transmitHistoryCols:any[] = [
+    { field: 'ad', header: '광고' },
+    { field: 'title', header: '광고명' },
+    { field: 'transmitCount', header: '전송수' },
+    { field: 'transmitDate', header: '전송일시' },
+    { field: 'open', header: '열람' },
+    { field: 'notOpen', header: '미열람' },
+    { field: 'failed', header: '실패' },
+  ];
+  public transmitHistoryRowData:any[] = [
+    { ad: 'http://str.gomgxp.com/thail/GXP/2018/fLayoutTest/KakaoTalk_Video_20170612_1744_48_996/KakaoTalk_Video_20170612_1744_48_9960001.jpg', title: '후기작성알림', transmitCount: '5', transmitDate: '2018-05-30 05:30:22', open: '4', notOpen: '3', failed: '2' },
+    { ad: 'http://str.gomgxp.com/thail/GXP/2018/fLayoutTest/KakaoTalk_Video_20170612_1744_48_996/KakaoTalk_Video_20170612_1744_48_9960001.jpg', title: '후기작성알림', transmitCount: '5', transmitDate: '2018-05-30 05:30:22', open: '4', notOpen: '3', failed: '2' },
+    { ad: 'http://str.gomgxp.com/thail/GXP/2018/fLayoutTest/KakaoTalk_Video_20170612_1744_48_996/KakaoTalk_Video_20170612_1744_48_9960001.jpg', title: '후기작성알림', transmitCount: '5', transmitDate: '2018-05-30 05:30:22', open: '4', notOpen: '3', failed: '2' },
+  ];
 
   constructor(private breadcrumbService: BreadcrumbService) {
     this.breadcrumbService.setItems([
