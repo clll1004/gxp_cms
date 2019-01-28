26d8a8423ab2f3b3de0d7e33e71ff6d2cb0b5cbf
chealin <clll1004@gomcorp.com>
2019-01-28 11:19:22 +0900

    imp: 개발자도구 플레이어 UI(6890)

--- src/app/demo/view/developer-tool/components/player-tool/player-tool.component.ts
+++ src/app/demo/view/developer-tool/components/player-tool/player-tool.component.ts
@@ -6,6 +6,7 @@ import { BreadcrumbService } from '../../../../../breadcrumb.service';
 
 @Component({
   selector: 'player-tool',
+  styleUrls: ['../developer-tool.component.css'],
   templateUrl: './player-tool.component.html'})
 
 export class PlayerToolComponent implements OnInit {
@@ -17,5 +18,28 @@ export class PlayerToolComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  highlighting(e) {
+    const tabSec = document.getElementById('tab');
+    for (let i = 0 ; i < 3 ; i += 1) {
+      tabSec.children[i].classList.remove('on');
+    }
+    const target = e.currentTarget;
+    target.classList.add('on');
+  }
+
+  copyClipboard() {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = document.getElementById('copyArea').innerText;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
   }
 }
