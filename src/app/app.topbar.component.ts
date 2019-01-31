bb5589575c8cc0f01e8aa722304fc59f0ad8aef2
chealin <clll1004@gomcorp.com>
2019-01-31 13:16:54 +0900

    fix: 로그인 등 UI 수정(7009)

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from './demo/service/login.service';
 import { AppComponent } from './app.component';
 import { CookieService } from '../app/demo/service/cookie.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
@@ -9,11 +10,21 @@ import { CookieService } from '../app/demo/service/cookie.service';
 })
 export class AppTopBarComponent implements OnInit {
   public user:string = '';
+  public isLayoutShow: boolean = false;
 
-  constructor(public app: AppComponent, private loginService: LoginService, private cookieService: CookieService) {}
+  constructor(public app: AppComponent, private loginService: LoginService, private cookieService: CookieService, private router: Router) {}
 
   ngOnInit() {
     this.cookieService.getCookie('usr_seq') !== '0' ? this.user = 'cms' : this.user = 'admin';
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/' || event.url === '/login' || event.url === '/new-password' || event.url === '/new-password-complete' || event.url === '/reset-password' || event.url === '/expiration' || event.url === '/new-account' || event.url === '/new-account-complete') {
+          this.isLayoutShow = false;
+        } else {
+          this.isLayoutShow = true;
+        }
+      }
+    });
   }
 
   logout() {
